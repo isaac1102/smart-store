6fd7b7a0c602f21a2096c5c49b328ec98a6e1126
euijin jeong <hitman1439@naver.com>
2021-06-30 00:29:58 +0900

    1229

--- src/main/java/com/project/smartstore/SmartStoreApplication.java
+++ src/main/java/com/project/smartstore/SmartStoreApplication.java
@@ -8,6 +8,7 @@ public class SmartStoreApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SmartStoreApplication.class, args);
+    System.out.println("hello world");
   }
 
 }
