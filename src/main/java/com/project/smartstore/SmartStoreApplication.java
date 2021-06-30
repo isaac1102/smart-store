089f1f547817acc4706178430970d7a77158cf8b
euijin jeong <hitman1439@naver.com>
2021-06-30 21:44:01 +0900

    build test

--- src/main/java/com/project/smartstore/SmartStoreApplication.java
+++ src/main/java/com/project/smartstore/SmartStoreApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartStoreApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SmartStoreApplication.class, args); 
+    SpringApplication.run(SmartStoreApplication.class, args);
+    System.out.println("hi");
   }
 
 }
