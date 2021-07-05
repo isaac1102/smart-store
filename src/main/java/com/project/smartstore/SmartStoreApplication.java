c84c73f4f0abedc61749c6a8a853f98730da21be
isaac1102 <hitman1439@naver.com>
2021-07-05 16:26:34 +0900

    asdf

--- src/main/java/com/project/smartstore/SmartStoreApplication.java
+++ src/main/java/com/project/smartstore/SmartStoreApplication.java
@@ -8,7 +8,7 @@ public class SmartStoreApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SmartStoreApplication.class, args);
-    System.out.println("12");
+    System.out.println("123");
   }
 
 }
